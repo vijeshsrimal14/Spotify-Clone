95bad107676281b2bf007786ec286c64aeaf77d2
vijeshsrimal14 <vijeshsrimal15@gmail.com>
2022-08-30 09:19:28 +0530

    Body done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -5,7 +5,7 @@ import { useStateProvider } from '../utils/StateProvider'
 import axios from "axios";
 import { reducerCases } from "../utils/Constants";
 
-export default function Body() {
+export default function Body(headerBackground) {
     const [{ token,selectedPlaylistId,selectedPlaylist },dispatch] = useStateProvider();
      
     useEffect(() => {
@@ -86,7 +86,7 @@ export default function Body() {
       };
 
       return (
-        <Container>
+        <Container headerBackground={headerBackground}> 
           {selectedPlaylist && (
             <>
               <div className="playlist">
@@ -174,6 +174,74 @@ export default function Body() {
           )}
         </Container>
     );
- }
+ } 
 
-const Container = styled.div``;
+ const Container = styled.div`
+ .playlist {
+   margin: 0 2rem;
+   display: flex;
+   align-items: center;
+   gap: 2rem;
+   .image {
+     img {
+       height: 15rem;
+       box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+     }
+   }
+   .details {
+     display: flex;
+     flex-direction: column;
+     gap: 1rem;
+     color: #e0dede;
+     .title {
+       color: white;
+       font-size: 4rem;
+     }
+   }
+ }
+ .list {
+   .header-row {
+     display: grid;
+     grid-template-columns: 0.3fr 3fr 2fr 0.1fr;
+     margin: 1rem 0 0 0;
+     color: #dddcdc;
+     position: sticky;
+     top: 15vh;
+     padding: 1rem 3rem;
+     transition: 0.3s ease-in-out;
+     background-color: ${({ headerBackground }) =>
+       headerBackground ? "#000000dc" : "none"};
+   }
+   .tracks {
+     margin: 0 2rem;
+     display: flex;
+     flex-direction: column;
+     margin-bottom: 5rem;
+     .row {
+       padding: 0.5rem 1rem;
+       display: grid;
+       grid-template-columns: 0.3fr 3.1fr 2fr 0.1fr;
+       &:hover {
+         background-color: rgba(0, 0, 0, 0.7);
+       }
+       .col {
+         display: flex;
+         align-items: center;
+         color: #dddcdc;
+         img {
+           height: 40px;
+           width: 40px;
+         }
+       }
+       .detail {
+         display: flex;
+         gap: 1rem;
+         .info {
+           display: flex;
+           flex-direction: column;
+         }
+       }
+     }
+   }
+ }
+`;
